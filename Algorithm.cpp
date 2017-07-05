4e5facbf8a29f3c8860859c8f8540631e2bf4199
邱世章 <qsz@qiushizhangdeMacBook-Pro.local>
2017-07-05 12:46:42 +0800

    test

--- Algorithm.cpp
+++ Algorithm.cpp
@@ -4,16 +4,39 @@
 using namespace std;
 
 void menu();
-void sub_menu1();
-void sub_menu2();
+void sub_menu();
+
 
 int main() {
-	menu();
-	return 0;
+    cout << "\t*************************************\n\n\t\t********欢迎********\n\n\t*************************************" << endl;
+    getchar();
+    menu();
+    return 0;
 }
 
 
 void menu(){
-	char choice;
-	cin >> choice;
+    system("clear");
+    char choice;
+    cout << "\t*************************************\n\n\t\t1、随机生成树 （3层）\n\n\t\t2、层序输入自定义树 （3层）\n\n\t*************************************" << endl;
+    cin >> choice;
+    switch (choice)
+    {
+        case '1':
+            sub_menu();
+            break;
+        case '2':
+            sub_menu();
+            break;
+        default:cout << "\t\t请正确选择!\n";
+            menu();
+    }
+}
+void sub_menu()
+{
+    char choice;
+    system("clear");
+    cout << "\t*************************************\n\n\t\t1、先序遍历二叉树\n\n\t\t2、中序遍历二叉树\n\n\t\t3、后序遍历二叉树\n\n\t\t4、层序遍历二叉树\n\n\t*************************************" << endl;
+    cin >> choice;
+    
 }
