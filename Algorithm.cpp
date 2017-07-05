6979199ab5a796f7bc4be9148609e6d59ecc187d
sunlanchang <sunlanchang@outlook.com>
2017-07-05 14:16:21 +0800

    add function file

--- Algorithm.cpp
+++ Algorithm.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <cstdio>
 #include <iostream>
 
 using namespace std;
@@ -6,37 +6,37 @@ using namespace std;
 void menu();
 void sub_menu();
 
-
-int main() {
-    cout << "\t*************************************\n\n\t\t********欢迎********\n\n\t*************************************" << endl;
-    getchar();
-    menu();
-    return 0;
+int main()
+{
+	cout << "\t*************************************\n\n\t\t********欢迎********\n\n\t*************************************" << endl;
+	getchar();
+	menu();
+	return 0;
 }
 
-
-void menu(){
-    system("clear");
-    char choice;
-    cout << "\t*************************************\n\n\t\t1、随机生成树 （3层）\n\n\t\t2、层序输入自定义树 （3层）\n\n\t*************************************" << endl;
-    cin >> choice;
-    switch (choice)
-    {
-        case '1':
-            sub_menu();
-            break;
-        case '2':
-            sub_menu();
-            break;
-        default:cout << "\t\t请正确选择!\n";
-            menu();
-    }
+void menu()
+{
+	system("clear");
+	char choice;
+	cout << "\t*************************************\n\n\t\t1、随机生成树 （3层）\n\n\t\t2、层序输入自定义树 （3层）\n\n\t*************************************" << endl;
+	cin >> choice;
+	switch (choice)
+	{
+	case '1':
+		sub_menu();
+		break;
+	case '2':
+		sub_menu();
+		break;
+	default:
+		cout << "\t\t请正确选择!\n";
+		menu();
+	}
 }
 void sub_menu()
 {
-    char choice;
-    system("clear");
-    cout << "\t*************************************\n\n\t\t1、先序遍历二叉树\n\n\t\t2、中序遍历二叉树\n\n\t\t3、后序遍历二叉树\n\n\t\t4、层序遍历二叉树\n\n\t*************************************" << endl;
-    cin >> choice;
-    
+	char choice;
+	system("clear");
+	cout << "\t*************************************\n\n\t\t1、先序遍历二叉树\n\n\t\t2、中序遍历二叉树\n\n\t\t3、后序遍历二叉树\n\n\t\t4、层序遍历二叉树\n\n\t*************************************" << endl;
+	cin >> choice;
 }
---
+++ function.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <cstdio>
+
+using namespace std;
+
+typedef char ElementType;
+
+//get random number function
+vector<char> random_number()
+{
+    vector<char> number = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
+    srand((unsigned)time(NULL)); //初始化随机数种子
+    for (int i = 0; i < 10; i++)
+    {
+        int index = 1 + rand() % 6;
+        char tmp;
+        tmp = number[index];
+        number[index] = number[0];
+        number[0] = tmp;
+    }
+    return number;
+}
+
+vector<char> random_number2()
+{
+    vector<char> number = random_number();
+    vector<char> number_res;
+    for (int i = 0, j = 0; i < 15; i++)
+    {
+        if (i == 3 || i == 4 || i == 6 || i == 7 || i == 10 || i == 11 || i == 13 || i == 14)
+        {
+            number_res.push_back(' ');
+        }
+        else
+        {
+            number_res.push_back(number[j]);
+            j++;
+        }
+    }
+    return number_res;
+}
+
+struct TreeNode
+{
+    ElementType data;
+    TreeNode *left;
+    TreeNode *right;
+};
+
+TreeNode *createTree()
+{
+    char c;
+    TreeNode *T = (TreeNode *)malloc(sizeof(TreeNode));
+    scanf("%c", &c);
+    if (' ' == c)
+    {
+        T = NULL;
+    }
+    else
+    {
+        T = (TreeNode *)malloc(sizeof(TreeNode));
+        T->data = c;
+        T->left = createTree();
+        T->right = createTree();
+    }
+    return T;
+}
+
+void pre_order(TreeNode *root)
+{
+    if (root != NULL)
+    {
+        cout << root->data << " ";
+        pre_order(root->left);
+        pre_order(root->right);
+    }
+}
+
+void in_order(TreeNode *root)
+{
+    if (root != NULL)
+    {
+        in_order(root->left);
+        cout << root->data << " ";
+        in_order(root->right);
+    }
+}
+
+void post_order(TreeNode *root)
+{
+    if (root != NULL)
+    {
+        cout << root->data << " ";
+        post_order(root->left);
+        post_order(root->right);
+    }
+}
+
+int main()
+{
+    TreeNode *T;
+    vector<char> number = random_number2();
+    for (auto e : number)
+    {
+        cout << e;
+    }
+    // cout << "\ninput: " << endl;
+    T = createTree();
+    cout << "前序遍历： " << endl;
+    pre_order(T);
+    cout << "\n中序遍历： " << endl;
+    in_order(T);
+    cout << "\n后序遍历： " << endl;
+    post_order(T);
+    cout << endl;
+    return 0;
+}
