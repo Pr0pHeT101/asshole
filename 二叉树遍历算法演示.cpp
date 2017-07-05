f5f4bb3e216227b5250516b18775526f139dbad7
邱世章 <qsz@qiushizhangdeMacBook-Pro.local>
2017-07-05 12:00:38 +0800

    test

---
+++ 二叉树遍历算法演示.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+void menu();
+void sub_menu1();
+void sub_menu2();
+
+int main() {
+	menu();
+	return 0;
+}
+
+
+void menu(){
+	char choice;
+	cin >> choice;
+}
